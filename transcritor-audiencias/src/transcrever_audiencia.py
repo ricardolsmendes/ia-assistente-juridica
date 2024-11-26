36b978042de1e779a56e9e24c7137fdf8723da3f
Ricardo Mendes <ricardolsmendes@gmail.com>
2024-11-26 11:45:15 -0300

    chore: traduzir o script transcrever_audiencia.py para pt-br

--- transcritor-audiencias/src/transcrever_audiencia.py
+++ transcritor-audiencias/src/transcrever_audiencia.py
@@ -7,75 +7,76 @@ import deepgram
 import dotenv
 import httpx
 
-dotenv.load_dotenv()
 
-
-def transcribe(audio_file_name: str) -> str:
-    # STEP 1 Create a Deepgram client using the API key in the environment variables
+def transcrever(nome_arquivo_audio: str) -> str:
+    # PASSO 1: Cria um cliente Deepgram usando a chave da API disponível nas variáveis
+    # de ambiente.
     dg_client = deepgram.DeepgramClient(os.getenv("DEEPGRAM_API_KEY"))
 
-    # STEP 2 Call the transcribe_file method on the prerecorded class
-    with open(audio_file_name, "rb") as audio_file:
-        buffer_data = audio_file.read()
+    # PASSO 2: Chama o transcribe_file para a audiência pré-gravada.
+    with open(nome_arquivo_audio, "rb") as arquivo_audio:
+        dados_buffer = arquivo_audio.read()
 
     payload: deepgram.FileSource = {
-        "buffer": buffer_data,
+        "buffer": dados_buffer,
     }
 
-    options = deepgram.PrerecordedOptions(
+    opcoes = deepgram.PrerecordedOptions(
         diarize=True, language="pt", model="whisper", smart_format=True, utterances=True
     )
 
-    response = dg_client.listen.prerecorded.v("1").transcribe_file(
-        payload, options, timeout=httpx.Timeout(300.0, connect=10.0)
+    resposta = dg_client.listen.prerecorded.v("1").transcribe_file(
+        payload, opcoes, timeout=httpx.Timeout(300.0, connect=10.0)
     )
 
-    results_file_name = f"{audio_file_name[:-4]}-whisper.json"
-    with open(results_file_name, "w", encoding="utf-8") as results_file:
-        results_file.write(response.to_json(ensure_ascii=False, indent=4))
+    nome_arquivo_resultados = f"{nome_arquivo_audio[:-4]}-whisper.json"
+    with open(nome_arquivo_resultados, "w", encoding="utf-8") as arquivo_resultados:
+        arquivo_resultados.write(resposta.to_json(ensure_ascii=False, indent=4))
 
-    return results_file_name
+    return nome_arquivo_resultados
 
 
-def convert_time(seconds: float):
-    return time.strftime("%H:%M:%S", time.gmtime(seconds))
+def formatar_marcacao_tempo(segundos: float):
+    return time.strftime("%H:%M:%S", time.gmtime(segundos))
 
 
 if __name__ == "__main__":
-    transcript_file_name = transcribe(sys.argv[1])
-    with open(transcript_file_name, "rb") as transcript_file:
-        transcript = json.loads(transcript_file.read())
+    dotenv.load_dotenv()
 
-    current_speaker = None
-    current_transcript_start = None
-    current_transcript = ""
-    text_file_name = f"{transcript_file_name[:-5]}-fmt.txt"
-    with open(text_file_name, "w") as text_file:
-        for utterance in transcript["results"]["utterances"]:
-            itr_speaker = utterance["speaker"]
+    nome_arquivo_transcricao = transcrever(sys.argv[1])
+    with open(nome_arquivo_transcricao, "rb") as arquivo_transcricao:
+        transcricao = json.loads(arquivo_transcricao.read())
 
-            if current_speaker is not None and itr_speaker != current_speaker:
-                text_file.write(
+    speaker_atual = None
+    inicio_transcricao_atual = None
+    transcricao_atual = ""
+    nome_arquivo_texto = f"{nome_arquivo_transcricao[:-5]}-fmt.txt"
+    with open(nome_arquivo_texto, "w") as arquivo_texto:
+        for fala in transcricao["results"]["utterances"]:
+            speaker_itr = fala["speaker"]
+
+            if speaker_atual is not None and speaker_itr != speaker_atual:
+                arquivo_texto.write(
                     f"\n\n"
-                    f"[speaker {current_speaker},"
-                    f" {convert_time(current_transcript_start)}]:"
+                    f"[falante {speaker_atual},"
+                    f" {formatar_marcacao_tempo(inicio_transcricao_atual)}]:"
                     f"\n"
                 )
-                text_file.write(current_transcript)
+                arquivo_texto.write(transcricao_atual)
 
-                current_transcript_start = None
-                current_transcript = ""
+                inicio_transcricao_atual = None
+                transcricao_atual = ""
 
-            if not current_transcript_start:
-                current_speaker = itr_speaker
-                current_transcript_start = utterance["start"]
+            if not inicio_transcricao_atual:
+                speaker_atual = speaker_itr
+                inicio_transcricao_atual = fala["start"]
 
-            current_transcript = f"{current_transcript} {utterance['transcript']}"
+            transcricao_atual = f"{transcricao_atual} {fala['transcript']}"
 
-        text_file.write(
+        arquivo_texto.write(
             f"\n\n"
-            f"[speaker {current_speaker},"
-            f" {convert_time(current_transcript_start)}]:"
+            f"[speaker {speaker_atual},"
+            f" {formatar_marcacao_tempo(inicio_transcricao_atual)}]:"
             f"\n"
         )
-        text_file.write(current_transcript)
+        arquivo_texto.write(transcricao_atual)
